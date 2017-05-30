274c2e49dcd441a34b487ec86a3af8e1356bc17c
Yannik Buerkle <mail@yannik-buerkle.de>
2017-05-31 00:00:21 +0200

    Prüfe auf google.de in example

--- example/simple.js
+++ example/simple.js
@@ -3,6 +3,6 @@ const getfavicon = require('../lib/getfavicon');
 
 get()
 async function get() {
-  let favicon = await getfavicon('https://yannik-buerkle.de/')
+  let favicon = await getfavicon('https://google.de/')
   console.log(favicon)
 }
